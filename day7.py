9e2858a72cbc5e8cb5a6970cb83ff70e102c482b
alextjanse <github@alexjanse.nl>
2024-12-08 12:58:53 +0100

    Added extra explanation

--- day7.py
+++ day7.py
@@ -23,11 +23,23 @@ def operator_combinations(operators: list[str], length: int) -> Iterator[list[st
     num_operators = len(operators)
     combination = [operators[0]] * length
 
-    '''There are n ** length number of combinations, where n is the number of operators.
+    '''There are n ^ length number of combinations, where n is the number of operators.
     If we loop over all those numbers and convert them to a base-n number, then each digit
     corresponds to a operator for each position in the combination. We can find the exact
     operator by doing a modulo with the number of operators, and then for the next position
-    we adjust by shifting to the next digit by deviding by n.'''
+    we adjust by shifting to the next digit by deviding by n.
+    
+    let n: number of operators
+    let l: length
+    let i: index in [0..n^l]
+    let o_i: index of operator i [0..l-1]
+
+    i = o_0 * n^0 + 0_1 * n^1 + ... + 0_(l-1)^(l-1)
+      = sum of i in [0..l-1]: o_i * n^i
+    i % n = o_0
+    i // n = (0_1 * n^1 + ... + 0_(l-1)^(l-1)) / n
+           = 0_1 * n^0 + ... + 0_(l-1)^(l-2)
+    note that o_0 * n^0 = o_0 < n, so it is removed in the integer devision.'''
     for index in range(num_operators**length):
         temp_index = index
         for i in range(length):
