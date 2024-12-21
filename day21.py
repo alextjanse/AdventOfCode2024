6e91ada90c0e5f66a99c9255b7655e913318e5d4
alextjanse <github@alexjanse.nl>
2024-12-21 09:03:36 +0100

    Day 21 part 1 rewritten

--- day21.py
+++ day21.py
@@ -1,25 +1,15 @@
 from heapq import heappop, heappush
 
 NUMERICAL_KEYPAD = {
-    (0, 0): '7',
-    (1, 0): '8',
-    (2, 0): '9',
-    (0, 1): '4',
-    (1, 1): '5',
-    (2, 1): '6',
-    (0, 2): '1',
-    (1, 2): '2',
-    (2, 2): '3',
-    (1, 3): '0',
-    (2, 3): 'A',
+    (0, 0): '7', (1, 0): '8', (2, 0): '9',
+    (0, 1): '4', (1, 1): '5', (2, 1): '6',
+    (0, 2): '1', (1, 2): '2', (2, 2): '3',
+                 (1, 3): '0', (2, 3): 'A',
 }
 
 DIRECTIONAL_KEYPAD = {
-    (1, 0): '^',
-    (2, 0): 'A',
-    (0, 1): '<',
-    (1, 1): 'v',
-    (2, 1): '>',
+                 (1, 0): '^', (2, 0): 'A',
+    (0, 1): '<', (1, 1): 'v', (2, 1): '>',
 }
 
 DIRECTIONS = {
@@ -33,56 +23,83 @@ def get_data() -> list[str]:
     with open('data/day21.txt', encoding='utf-8') as file:
         return file.read().splitlines()
 
-def get_shortest_seq_length(code: str) -> int:
-    unhandled_states = [(0, '', (2, 0), '', (2, 0), '', (2, 3), '')]
+def get_shortest_seq_length(code: str, directional_robots: int) -> int:
+    '''Find the shortest sequence of input using BFS.'''
+    unhandled_states = [(0, '', [(2, 0)] * directional_robots, (2, 3))]
     handled_states = set()
 
     while unhandled_states:
-        t, in_0, pos_1, in_1, pos_2, in_2, pos_3, in_3 = heappop(unhandled_states)
+        t, out, directional_pos, numerical_pos = heappop(unhandled_states)
 
-        if (in_3, pos_1, pos_2, pos_3) in handled_states:
+        state_check = (out, tuple(directional_pos), numerical_pos)
+
+        if state_check in handled_states:
             continue
-        handled_states.add((in_3, pos_1, pos_2, pos_3))
+        handled_states.add(state_check)
 
-        if in_3 == code:
+        if out == code:
             return t
         
         # I press direction
-        for d, (dx, dy) in DIRECTIONS.items():
-            x, y = pos_1[0] + dx, pos_1[1] + dy
+        for dx, dy in DIRECTIONS.values():
+            x, y = directional_pos[0]
+            x, y = x + dx, y + dy
             if (x, y) in DIRECTIONAL_KEYPAD:
-                heappush(unhandled_states, (t + 1, in_0 + d, (x, y), in_1, pos_2, in_2, pos_3, in_3))
+                heappush(unhandled_states, (t + 1, out, [(x, y)] + directional_pos[1:], numerical_pos))
 
-        # I press A
-        action_1 = DIRECTIONAL_KEYPAD[pos_1]
-        if action_1 in DIRECTIONS: # Robot 1 presses direction
-            dx, dy = DIRECTIONS[action_1]
-            x, y = pos_2[0] + dx, pos_2[1] + dy
-            if (x, y) in DIRECTIONAL_KEYPAD:
-                heappush(unhandled_states, (t + 1, in_0 + 'A', pos_1, in_1 + action_1, (x, y), in_2, pos_3, in_3))
-        else: # Robot 1 presses A
-            action_2 = DIRECTIONAL_KEYPAD[pos_2]
-            if action_2 in DIRECTIONS: # Robot 2 presses direction
-                dx, dy = DIRECTIONS[action_2]
-                x, y = pos_3[0] + dx, pos_3[1] + dy
-                if (x, y) in NUMERICAL_KEYPAD:
-                    heappush(unhandled_states, (t + 1, in_0 + 'A', pos_1, in_1 + action_1, pos_2, in_2 + action_2, (x, y), in_3))
-            else: # Robot 2 presses A
-                action_3 = NUMERICAL_KEYPAD[pos_3]
-                if action_3 != code[len(in_3)]:
-                    continue
-                heappush(unhandled_states, (t + 1, in_0 + 'A', pos_1, in_1 + action_1, pos_2, in_2 + action_2, pos_3, in_3 + action_3))
+        # I Press A
+
+        # Use a copy of the directions for the new states
+        new_directional_pos = directional_pos[:]
+
+        i = 0
+        while i < len(directional_pos):
+            action = DIRECTIONAL_KEYPAD[directional_pos[i]]
+            if action in DIRECTIONS:
+                dx, dy = DIRECTIONS[action]
+
+                if i + 1 < len(directional_pos):
+                    x, y = directional_pos[i + 1]
+                    x, y = x + dx, y + dy
+                    if (x, y) in DIRECTIONAL_KEYPAD:
+                        new_directional_pos[i + 1] = x, y
+                        heappush(unhandled_states, (t + 1, out, new_directional_pos, numerical_pos))
+                else:
+                    x, y = numerical_pos
+                    x, y = x + dx, y + dy
+                    if (x, y) in NUMERICAL_KEYPAD:
+                        heappush(unhandled_states, (t + 1, out, new_directional_pos, (x, y)))
+                
+                break
+            else: # Action is A
+                i += 1
+        
+        if i == len(directional_pos):
+            # Numerical keypad is being pressed
+            action = NUMERICAL_KEYPAD[numerical_pos]
+            if action != code[len(out)]:
+                continue
+            heappush(unhandled_states, (t + 1, out + action, new_directional_pos, numerical_pos))
 
 def solve_part_1(codes: list[str]) -> None:
     complexity_sum = 0
 
     for code in codes:
-        shortest_seq_length = get_shortest_seq_length(code)
+        shortest_seq_length = get_shortest_seq_length(code, 2)
         numeric_part = int(code[:-1])
         complexity_sum += shortest_seq_length * numeric_part
     
     print(complexity_sum)
 
+def solve_part_2(codes: list[str]) -> None:
+    complexity_sum = 0
+
+    for code in codes:
+        pass
+
+    print(complexity_sum)
+
 if __name__ == '__main__':
     codes = get_data()
     solve_part_1(codes)
+    solve_part_2(codes)
